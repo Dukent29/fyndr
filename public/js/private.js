8f1f06053632f61d4bbe74036c7614d2a4981d04
Kevin Bruce Mulinda <89967620+Dukent29@users.noreply.github.com>
2025-05-28 18:06:24 +0200

    Update private.js

--- public/js/private.js
+++ public/js/private.js
@@ -2,13 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const logoutBtn = document.getElementById('logoutBtn');
 
   logoutBtn.addEventListener('click', async () => {
-    // Optional: send a request to backend if you want
-    // await fetch('http://localhost:3000/api/users/logout', { method: 'POST' });
+    
 
-    // Remove token from localStorage
+    
     localStorage.removeItem('token');
 
-    // Optionally redirect to login or home page
-    window.location.href = './index.html'; // adjust path as needed
+    
+    window.location.href = './index.html';
   });
 });
